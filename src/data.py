b95cbbbe7dd1ba139dfe41761952bacd83d9671b
FranciscoVogt <franciscogermanovogt@gmail.com>
2023-09-04 17:46:03 -0300

    updateTh

--- src/data.py
+++ src/data.py
@@ -8,18 +8,27 @@ class generator:
 		self.generation_port = 68
 		self.output_port = 0
 		self.channel = 0
+		self.port_bw = ''
+		self.throughput_defined = False
+		self.throughput = 0
+		self.throughput_mode = '' #meter or port_shaping
 	
 	def addGenerationPort(self, port):
 		self.generation_port = port
 	
-	def addOutputPort(self, port, channel):
+	def addOutputPort(self, port, channel, bw):
 		self.output_port = port
 		self.channel = channel
+		self.port_bw = bw
 
+	def addThroughput(self, throughput, mode):
+		self.throughput_defined = True
+		self.throughput = throughput
+		self.throughput_mode = mode
 
 	def generate(self):
-		generatePy(self.generation_port, self.channel)
-		generateP4()
+		generatePy(self.generation_port, self.channel, self.throughput_defined, self.throughput_mode, self.throughput)
+		generateP4(self.throughput_defined, self.throughput_mode)
 		generateHeader()
 		generateUtil()
-		generatePortConfig(self.output_port)
+		generatePortConfig(self.output_port, self.port_bw)
