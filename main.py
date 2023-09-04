c562d7c14eb221e08a71f30357b4d5642e159619
FranciscoVogt <franciscogermanovogt@gmail.com>
2023-09-04 17:45:35 -0300

    updateTh

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from src.data import *
 Gerador = generator("pipo")
 
 Gerador.addGenerationPort(68)
-Gerador.addOutputPort(1, 128) #Physical Port, Port ID(D_P)
+Gerador.addOutputPort(1, 128, "10G") #Physical Port, Port ID(D_P), Port bw
 Gerador.generate()
